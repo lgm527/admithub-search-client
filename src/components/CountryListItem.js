78daca7513305f3df1d1cc499702df37fc49cb2b
lgm527 <laurellmccaffrey@gmail.com>
2020-10-09 15:09:58 -0400

    part1 added

---
+++ src/components/CountryListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CountryListItem = () => {
+    return ( 
+        <li className="list-group-item" style={{width:'50%'}}>
+          <img className="media-object" alt="flag" src="https://restcountries.eu/data/usa.svg" style={{width:'20px'}}/>
+          
+        CountryNamePlaceholder
+
+        <button>+</button>
+      </li>
+     );
+}
+ 
+export default CountryListItem;
