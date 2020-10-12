f4670fac9b093cb2bdc1fcf7f2201c0591fe2e71
lgm527 <laurellmccaffrey@gmail.com>
2020-10-12 12:59:10 -0400

    hideous inline styles added

--- src/components/CountryListItem.js
+++ src/components/CountryListItem.js
@@ -4,19 +4,19 @@ const CountryListItem = (props) => {
     return ( 
       props.add ?
         <li className="list-group-item">
-        <img className="media-object" alt="flag" src={props.country.flag} style={{width:'20px'}}/>
+        <img className="media-object" alt="flag" src={props.country.flag} style={{width:'20px', float:'left'}}/>
         
         {props.country.name}
 
-        <button onClick={() => props.add(props.country)}>+</button>
+        <button onClick={() => props.add(props.country)} style={{float:'right'}}>+</button>
         </li>
       :
         <li className="list-group-item">
-        <img className="media-object" alt="flag" src={props.country.flag} style={{width:'20px'}}/>
+        <img className="media-object" alt="flag" src={props.country.flag} style={{width:'20px', float:'left'}}/>
         
         {props.country.name}
 
-        <button onClick={() => props.remove(props.country)}>x</button>
+        <button onClick={() => props.remove(props.country)} style={{float:'right'}}>x</button>
         </li>
      );
 }
